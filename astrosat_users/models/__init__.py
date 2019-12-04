e3d95e312425b8d4236d03ccb8d769253f8ea2c8
Allyn Treshansky <allyn.treshansky@gmail.com>
2019-12-04 11:43:07 +0000

    added missing import

--- astrosat_users/models/__init__.py
+++ astrosat_users/models/__init__.py
@@ -1,4 +1,4 @@
-from .models_profiles import PROFILES, get_profile_qs, UserProfileField
+from .models_profiles import UserProfileField, PROFILES, get_profile_qs
 from .models_roles import UserRole, UserPermission
 from .models_settings import UserSettings
-from .models_users import User
+from .models_users import User, get_sentinel_user
