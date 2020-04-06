9195f493636698edc281d1baa72c40fcf8b738ae
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-04-06 15:21:09 +0100

    feat(backend): admin action to add/remove roles
    Admin action to add/remove roles in bulk via an admin action.

--- astrosat_users/admin/admin_users.py
+++ astrosat_users/admin/admin_users.py
@@ -1,7 +1,11 @@
+from django import forms
+from django.forms import ValidationError
+from django.conf import settings
 from django.contrib import admin, messages
 from django.contrib.auth import admin as auth_admin
-
 from django.contrib.auth.models import Group, Permission
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 from astrosat.admin import get_clickable_m2m_list_display
 
@@ -36,59 +40,13 @@ class UserRoleAdmin(admin.ModelAdmin):
     filter_horizontal = ("permissions",)
 
 
-def toggle_approval(model_admin, request, queryset):
-    # TODO: doing this cleverly w/ negated F expressions is not supported (https://code.djangoproject.com/ticket/17186)
-    # queryset.update(is_approved=not(F("is_approved")))
-    for obj in queryset:
-        obj.is_approved = not obj.is_approved
-        obj.save()
-
-        msg = f"{obj} {'not' if not obj.is_approved else ''} approved."
-        messages.add_message(request, messages.INFO, msg)
-
-
-toggle_approval.short_description = "Toggles the approval of the selected users"
-
-
-def toggle_verication(model_admin, request, queryset):
-
-    for obj in queryset:
-
-        emailaddress, created = obj.emailaddress_set.get_or_create(
-            user=obj, email=obj.email
-        )
-        if not emailaddress.primary:
-            emailaddress.set_as_primary(conditional=True)
-
-        emailaddress.verified = not emailaddress.verified
-        emailaddress.save()
-
-        msg = f"{emailaddress} {'created and' if created else ''} {'not' if not emailaddress.verified else ''} verified."
-        messages.add_message(request, messages.INFO, msg)
-
-
-toggle_verication.short_description = (
-    "Toggles the verification of the selected users' primary email addresses"
-)
-
-
-def logout_all(model_admin, request, queryset):
-    for obj in queryset:
-        obj.logout_all()
-
-        msg = f"logged {obj} out of all sessions."
-        messages.add_message(request, messages.INFO, msg)
-
-
-logout_all.short_description = "Logs the selected users out of all active sessions"
-
-
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
 
+    actions = ("update_roles", "toggle_approval", "toggle_verication", "logout_all")
+
     form = UserAdminChangeForm
     add_form = UserAdminCreationForm
-    actions = (toggle_approval, toggle_verication, logout_all)
     fieldsets = (
         (
             "User",
@@ -132,3 +90,108 @@ class UserAdmin(auth_admin.UserAdmin):
 
     is_verified_pretty.boolean = True
     is_verified_pretty.short_description = "IS VERIFIED"
+
+    ###########
+    # actions #
+    ###########
+
+    def toggle_approval(self, request, queryset):
+        # TODO: doing this cleverly w/ negated F expressions is not supported (https://code.djangoproject.com/ticket/17186)
+        # queryset.update(is_approved=not(F("is_approved")))
+        for obj in queryset:
+            obj.is_approved = not obj.is_approved
+            obj.save()
+
+            msg = f"{obj} {'not' if not obj.is_approved else ''} approved."
+            self.message_user(request, msg)
+
+    toggle_approval.short_description = "Toggles the approval of the selected users"
+
+    def toggle_verication(self, request, queryset):
+
+        for obj in queryset:
+
+            emailaddress, created = obj.emailaddress_set.get_or_create(
+                user=obj, email=obj.email
+            )
+            if not emailaddress.primary:
+                emailaddress.set_as_primary(conditional=True)
+
+            emailaddress.verified = not emailaddress.verified
+            emailaddress.save()
+
+            msg = f"{emailaddress} {'created and' if created else ''} {'not' if not emailaddress.verified else ''} verified."
+            self.message_user(request, sg)
+
+    toggle_verication.short_description = (
+        "Toggles the verification of the selected users' primary email addresses"
+    )
+
+    def logout_all(self, request, queryset):
+        for obj in queryset:
+            obj.logout_all()
+
+            msg = f"logged {obj} out of all sessions."
+            self.message_user(request, msg)
+
+    logout_all.short_description = "Logs the selected users out of all active sessions"
+
+    def update_roles(self, request, queryset):
+        class _UpdateRolesForm(forms.Form):
+            _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+            included_roles = forms.ModelMultipleChoiceField(
+                required=False, label="Included Roles", queryset=UserRole.objects.all(),
+            )
+            excluded_roles = forms.ModelMultipleChoiceField(
+                required=False, label="Excluded Roles", queryset=UserRole.objects.all()
+            )
+
+            def clean(self):
+                cleaned_data = super().clean()
+                included_roles = cleaned_data["included_roles"]
+                excluded_roles = cleaned_data["excluded_roles"]
+                if included_roles.intersection(excluded_roles).exists():
+                    raise ValidationError(
+                        "Included and excluded roles must be mutually-exclusive."
+                    )
+
+        update_roles_form = _UpdateRolesForm(
+            request.POST or None,
+            initial={
+                "_selected_action": request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+            },
+        )
+
+        if "apply" in request.POST:
+            if update_roles_form.is_valid():
+                included_roles = update_roles_form.cleaned_data["included_roles"]
+                excluded_roles = update_roles_form.cleaned_data["excluded_roles"]
+
+                for obj in queryset:
+
+                    existing_roles = obj.roles.all()
+                    roles_to_add = included_roles.difference(existing_roles)
+                    roles_to_remove = excluded_roles.intersection(existing_roles)
+                    obj.roles.add(*roles_to_add)
+                    obj.roles.remove(*roles_to_remove)
+
+                    if roles_to_add.exists() or roles_to_remove.exists():
+                        msg = f"Successfully added '{', '.join([r.name for r in roles_to_add])}' and removed '{', '.join([r.name for r in roles_to_remove])}' from '{obj}'."
+                        self.message_user(request, msg)
+
+                return HttpResponseRedirect(request.get_full_path())
+
+        context = {
+            "form": update_roles_form,
+            "users": get_clickable_m2m_list_display(User, queryset),
+            "site_header": getattr(settings, "ADMIN_SITE_HEADER", None),
+            "site_title": getattr(settings, "ADMIN_SITE_TITLE", None),
+            "index_title": getattr(settings, "ADMIN_INDEX_TITLE", None),
+        }
+        return render(
+            request, "astrosat_users/admin/update_roles.html", context=context
+        )
+
+    update_roles.short_description = (
+        "Updates the selected users to include/exclude roles"
+    )
