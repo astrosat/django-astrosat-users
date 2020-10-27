000ea188a1af3dd30913e3d2f4d80bb593a734de
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-10-27 13:42:26 +0000

    chore(backend): fixed typo
    IssueID #114

--- astrosat_users/adapters.py
+++ astrosat_users/adapters.py
@@ -306,7 +306,7 @@ class AccountAdapter(AdapterMixin, DefaultAccountAdapter):
                 raise TypeError('"bcc" argument must be a list or tuple')
             self.bcc = list(bcc)
         msg = self.render_mail(template_prefix, email, context)
-        msg.cc = [address for address in bcc if address != email]
+        msg.cc = [address for address in cc if address != email]
         msg.bcc = [address for address in bcc if address != email]
         msg.send()
 
