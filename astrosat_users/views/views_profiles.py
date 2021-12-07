d8729b9e42703fd057d9ccba0fd31006b9c004ce
Allyn Treshansky <allyn.treshansky@gmail.com>
2021-12-07 15:30:26 +0000

    chore(backend): addressed  errors in swagger schema generation and updated some swagger documentation

--- astrosat_users/views/views_profiles.py
+++ astrosat_users/views/views_profiles.py
@@ -1,10 +1,16 @@
+from collections import OrderedDict
+
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
 from django.utils.functional import cached_property
 
-from rest_framework import generics
+from rest_framework import generics, serializers, status
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import IsAuthenticated, BasePermission
+
+from drf_yasg2 import openapi
+from drf_yasg2.utils import swagger_auto_schema
 
 from astrosat.decorators import swagger_fake
 
@@ -21,6 +27,22 @@ class IsAdminOrSelf(BasePermission):
         return user.is_superuser or user == view.user
 
 
+auto_schema_kwargs = {
+    "responses": {
+        status.HTTP_200_OK:
+            openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties=OrderedDict((
+                    ("key", openapi.Schema(type=openapi.TYPE_STRING, example="value")),
+                ))
+            )
+    }
+}  # yapf: disable
+
+
+@method_decorator(swagger_auto_schema(**auto_schema_kwargs), name="get")
+@method_decorator(swagger_auto_schema(**auto_schema_kwargs), name="put")
+@method_decorator(swagger_auto_schema(**auto_schema_kwargs), name="patch")
 class UserProfileView(generics.RetrieveUpdateAPIView):
     """
     API to retrive/update a specific profile; Even though all profiles are output
@@ -35,6 +57,7 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
         user = get_object_or_404(get_user_model(), uuid=user_id)
         return user
 
+    @swagger_fake(serializers.Serializer)
     def get_serializer_class(self):
         obj = self.get_object()
         return obj.get_serializer_class()
