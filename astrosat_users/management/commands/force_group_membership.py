35c0e0aa4eb774eaada594acf2ee86aacbd94726
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-08-09 10:30:08 +0100

    feat(backend): updated force_group_membership command to support regex

--- astrosat_users/management/commands/force_group_membership.py
+++ astrosat_users/management/commands/force_group_membership.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand, CommandError
@@ -27,10 +29,18 @@ class Command(BaseCommand):
             help="The names of groups to join."
         )
 
+        parser.add_argument(
+            "--regex",
+            dest="is_regex",
+            action="store_true",
+            help="Whether or not to treat the group_names as regexs",
+        )
+
     def handle(self, *args, **options):
 
         username = options["username"]
         group_names = options["group_names"]
+        is_regex = options["is_regex"]
 
         try:
             UserModel = get_user_model()
@@ -39,12 +49,20 @@ class Command(BaseCommand):
             msg = f"Unable to find user '{username}'."
             raise CommandError(msg)
 
+        if is_regex:
+            filter_expr = "name__iregex"
+        else:
+            filter_expr = "name__iexact"
+
         groups = []
         for group_name in group_names:
-            try:
-                groups.append(Group.objects.get(name__iexact=group_name))
-            except Group.DoesNotExist:
-                msg = f"Unable to find group '{group_name}'."
+            filtered_groups = Group.objects.filter(**{filter_expr: group_name})
+            if not filtered_groups.exists():
+                msg = f"Unable to find any groups matching '{group_name}'"
                 raise CommandError(msg)
+            groups.append(filtered_groups)
 
-        user.groups.add(*groups)
+        user.groups.add(*chain.from_iterable(groups))
+        self.stdout.write(
+            f"successfully assigned user '{user}' to groups: '{', '.join([group.name for group in chain.from_iterable(groups)])}'."
+        )
